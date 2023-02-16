a1e0eb52f3bf28e7edc455546a1e78356b7097dd
Mgolik16983 <mgolik1983@gmail.com>
2023-02-16 22:58:59 +0300

    Task 1

--- 1_1.py
+++ 1_1.py
@@ -1,4 +1,9 @@
 #Пользователь вводит предложение, заменить все пробелы на "-" 2-мя
 #способами
+# - 1 -
 sentence = input('Введите предложение: ')
-print(sentence.split())
+print(sentence.replace(' ','-'))
+
+# - 2 -
+sentence = input('Введите предложение: ')
+print('-'.join(sentence.split()))
