b0b9c74b94f3c494044dbef5943780b677b38989
Mgolik16983 <mgolik1983@gmail.com>
2023-02-19 00:30:53 +0300

    Task 1

---
+++ 3_3.py
@@ -0,0 +1,5 @@
+Name = input('Введите ваше имя: ')
+Age = input('Введите ваш возраст: ')
+City = input('Введите ваш город: ')
+print(f'Меня зовут {Name}. Мне {Age}. Я живу в городе {City}'.format(Name=Name, Age=Age, City=City))
+print(f'Меня зовут {Name}. Мне {Age}. Я живу в городе {City}')
