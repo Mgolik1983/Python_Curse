313cb78366fcdf2f28ad549b67fbbbfe0572816d
Mgolik16983 <mgolik1983@gmail.com>
2023-02-19 00:12:28 +0300

    Task 2

--- 2_2.py
+++ 2_2.py
@@ -1,4 +1,13 @@
-numbers = (int(input('Введите первое слагаемое: ')),
-           int(input('Введите второе слагаемое: ')),
-           int(input('Введите третье слагаемое: ')))
+number_one = int(input('Введите первое слагаемое: '))
+number_two = int(input('Введите второе слагаемое: '))
+number_three = int(input('Введите третье слагаемое: '))
+print((number_one+number_two+number_three)/3)
+
+
+
+
+number_one
+numbers = int((input('Введите первое слагаемое: '),
+           input('Введите второе слагаемое: '),
+           input('Введите третье слагаемое: ')))
 print(round(sum(numbers)/len(numbers), 3))
