e3e121ee94d74769f11b915f1d71c5d12e586c77
Mgolik16983 <mgolik1983@gmail.com>
2023-02-18 23:02:58 +0300

    Task 2

--- 2_2.py
+++ 2_2.py
@@ -1,4 +1,7 @@
-number_one = int(input('Введите первое слагаемое: '))
-number_two = int(input('Введите второе слагаемое: '))
-number_three = int(input('Введите третье слагаемое: '))
-print(sum.)
+#number_one = int(input('Введите первое слагаемое: '))
+#number_two = int(input('Введите второе слагаемое: '))
+#number_three = int(input('Введите третье слагаемое: '))
+#print((number_one+number_two+number_three)/3)
+
+numbers = (input('Введите первое слагаемое: '), input('Введите второе слагаемое: '), input('Введите третье слагаемое: '))
+prin(len(numbers))
