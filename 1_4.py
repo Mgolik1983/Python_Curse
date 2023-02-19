9bd798fc953f2910904fd49ca648ba171b9dd720
Mgolik16983 <mgolik1983@gmail.com>
2023-02-19 20:50:55 +0300

    Task 1

--- 1_4.py
+++ 1_4.py
@@ -1,19 +1,24 @@
-number_one = int(input('Введите первое число: '))
-number_two = int(input('Введите второе число: '))
-number_three = int(input('Введите третье число: '))
-count_positive_numbers = 0
-count_negative_numbers = 0
-if number_one > 0:
-    count_positive_numbers += 1
-else:
-    count_negative_numbers += 1
-if number_two > 0:
-    count_positive_numbers += 1
-else:
-    count_negative_numbers += 1
-if number_three > 0:
-    count_positive_numbers += 1
-else:
-    count_negative_numbers += 1
-print(f'Количество положительных чисел: {count_positive_numbers}, ',
-      f'количество отрицательных чисел: {count_negative_numbers}')
+numbers = (int(input('Введите первое число: ')),
+           int(input('Введите второе число: ')),
+           int(input('Введите третье число: ')))
+
+# number_one = int(input('Введите первое число: '))
+# number_two = int(input('Введите второе число: '))
+# number_three = int(input('Введите третье число: '))
+# count_positive_numbers = 0
+# count_negative_numbers = 0
+# if number_one > 0:
+#    count_positive_numbers += 1
+# else:
+#    count_negative_numbers += 1
+# if number_two > 0:
+#    count_positive_numbers += 1
+# else:
+#    count_negative_numbers += 1
+# if number_three > 0:
+#    count_positive_numbers += 1
+# else:
+#    count_negative_numbers += 1
+# print(f'Количество положительных чисел: {count_positive_numbers}, ',
+#       f'количество отрицательных чисел: {count_negative_numbers}')
+
