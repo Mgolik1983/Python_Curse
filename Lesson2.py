ece70aee57bb2f517ea7135e25b2e4dddfe95dc6
Mgolik16983 <mgolik1983@gmail.com>
2023-02-20 19:25:16 +0300

    задание с циклом, лекция

--- Lesson2.py
+++ Lesson2.py
@@ -7,18 +7,18 @@
 # max_num = max(a)
 # print(max_num)
 
-m = [25, 10, 5, 1]
+# m = [25, 10, 5, 1]
 #print(sum(m))
 #sum_money = input('Sum: ')
 #while
 
 
 
-limit_summ = input(limit_sum)
-proc = input(proc)
+limit_summ = int(input('limit_sum: '))
+proc = float(input('proc: '))
+target_sum = limit_summ*2
 year = 0
-while limit_summ != 2*limit_summ
+while limit_summ < target_sum:
     year += 1
-    limit_summ += limit_summ/100*proc
-print (year)
-
+    limit_summ += limit_summ*proc/100
+print(year)
