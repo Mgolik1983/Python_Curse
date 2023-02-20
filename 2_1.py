77fe37b83f171d2d3c520b615aef844a0cc657e9
Mgolik16983 <mgolik1983@gmail.com>
2023-02-20 22:43:24 +0300

    Task 2_1

--- 2_1.py
+++ 2_1.py
@@ -1,4 +1,4 @@
 # Заполнить список степенями числа 2 (от 2^1 до 2^n)
 n = int(input('Введите n степеней: '))
-numbers = [2**n for n in range(1, n+1, 1)]
+numbers = [2**n for n in range(1, n+1)]
 print(numbers)
