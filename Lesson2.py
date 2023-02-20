c5e606e109aeb1b86f96d8e7474f85a6a61ce6db
Mgolik16983 <mgolik1983@gmail.com>
2023-02-20 19:17:28 +0300

    задание с циклом, лекция

--- Lesson2.py
+++ Lesson2.py
@@ -1,5 +1,24 @@
-a = input('Enter a:')
-while not a.isdigit():
-    a = input('Enter a:')
-print(a)
+# a = input('Enter a:')
+# while not a.isdigit():
+#     a = input('Enter a:')
+# print(a)
+
+# a = input('a: ')
+# max_num = max(a)
+# print(max_num)
+
+m = [25, 10, 5, 1]
+#print(sum(m))
+#sum_money = input('Sum: ')
+#while
+
+
+
+limit_summ = input(limit_sum)
+proc = input(proc)
+year = 0
+while limit_summ != 2*limit_summ
+    year += 1
+    limit_summ += limit_summ/100*proc
+print (year)
 
