cab6663d4c708ba34eab2ef7d8b36f6c2df6878b
Mgolik16983 <mgolik1983@gmail.com>
2023-02-22 13:35:25 +0300

    Task 2_3

--- 2_3.py
+++ 2_3.py
@@ -3,7 +3,7 @@
 # для этих ключей будут браться с клавиатуры
 # {1: {'name': None,'email': None}, }
 n = int(input('Введите количество пользователей: '))
-users_n = dict.fromkeys([1 + n for n in range(n)], {'name', 'email'})
+users_n = dict.fromkeys([i for i in range(n)], {'name', 'email'})
 for i in users_n:
     users_n[i] = {'name': input('Name: '), 'email': input('Email: ')}
 print(users_n)
