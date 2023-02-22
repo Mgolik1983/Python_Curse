78d10502111724ff6a8116476118b79dabe5c85c
Mgolik16983 <mgolik1983@gmail.com>
2023-02-22 13:24:43 +0300

    Task 2_2

--- 2_3.py
+++ 2_3.py
@@ -1,14 +1,9 @@
 # Заполнить словарь где ключами будут выступать числа от 0 до n, а
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
-# {1: {'name': None,
-#      'email': None} }
-import  collections
-n =int(input('n: '))
-list_n = [1 + n for n in range(n)]
-users_n = dict.fromkeys(list_n)
-# for i in range(users_n)
-   
-print(list_n)
+# {1: {'name': None,'email': None}, }
+n = int(input('Введите количество пользователей: '))
+users_n = dict.fromkeys([1 + n for n in range(n)], {'name', 'email'})
+for i in users_n:
+    users_n[i] = {'name': input('Name: '), 'email': input('Email: ')}
 print(users_n)
-
