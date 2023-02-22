6eb732d83ba5129641b0b07a6e2d84600cabd33a
Mgolik16983 <mgolik1983@gmail.com>
2023-02-22 09:50:29 +0300

    Task 2_2

--- 2_2.py
+++ 2_2.py
@@ -2,7 +2,7 @@
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
 sentence = input('Введите предложение: ')
-dict_chars = dict.fromkeys(sentence, 0)
+dict_chars = dict.fromkeys(sentence,0)
 for i in sentence:
     dict_chars[i] += 1
 print(dict_chars)
