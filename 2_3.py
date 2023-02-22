bab240e7ccb2ce65b45bfb2b9897fcd817a89c22
Mgolik16983 <mgolik1983@gmail.com>
2023-02-22 09:46:22 +0300

    Task 2_3

--- 2_3.py
+++ 2_3.py
@@ -1,15 +1,14 @@
 # Заполнить словарь где ключами будут выступать числа от 0 до n, а
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
-# [1: ['name': None, 'email': None], ]
-import collections
-data1 {
-    'a': 1,
-    'b': 2
-}
-data2 {
-    'c': 3,
-    'd': 4
-  }
-chain = collections.ChainMap (data1,data2)
-print(chain)
+# {1: {'name': None,
+#      'email': None} }
+import  collections
+n =int(input('n: '))
+list_n = [1 + n for n in range(n)]
+users_n = dict.fromkeys(list_n)
+# for i in range(users_n)
+   
+print(list_n)
+print(users_n)
+
