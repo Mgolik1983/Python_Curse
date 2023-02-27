49aa33938782cb3bf498a310b708473cc2b76bfe
Mgolik16983 <mgolik1983@gmail.com>
2023-02-27 21:31:55 +0300

    Lessson 4

--- lesson4_2.py
+++ lesson4_2.py
@@ -1,14 +1,15 @@
 class Numbers:
-    def __init__(self, numbers):
-        self.numbers = numbers
+    def __init__(self, *args):
+        self.numbers = args
     def average(self):
         return sum(self.numbers) / len(self.numbers)
     def max_count(self):
         from collections import Counter
         counter = Counter(self.numbers)
         max_common = counter.most_common(1)[0][1]
-        numbers = [*filter(lambda  x: self.numbers.count(x) == max_common, self.numbers)]
+        numbers = [*filter(lambda x: self.numbers.count(x) == max_common, self.numbers)]
         return sum(numbers) / len(numbers)
 
-n = numbers(2,5,8,9,7,8,9,8,9,5,8)
+
+n = Numbers(2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 2, 2, 3, 3)
 print(n.average())
