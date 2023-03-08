dbae1cb080eab23a821aedfd42ea0aa64b07f84e
Mgolik16983 <mgolik1983@gmail.com>
2023-03-08 14:12:43 +0300

    Merge remote-tracking branch 'origin/main'

--- 2_4.py
+++ 2_4.py
@@ -1,14 +1,10 @@
 # Вывести первые N цисел кратные M и больше K
 numbers = [2, 3, 5, 7, 89, 4, 9, 7, 6, 54, 87, 69, 32, 33, 22, 34, 56, 38]
-# print(len(numbers))
-result = []
-count = 0
+counter = 0
 n = int(input('Input n: '))
 m = int(input('Input m: '))
 k = int(input('Input k: '))
-# print (n)
-# print(m)
-
 for number in numbers:
-    if number % m == 0 and number > k:
+    if number % m == 0 and number > k and counter < n:
+        counter += 1
         print(number)
