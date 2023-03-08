e4971e0d91d53e7584d8c7579fa11595ce18551d
Mgolik16983 <mgolik1983@gmail.com>
2023-03-08 13:41:12 +0300

    Task 2_4

---
+++ 2_4.py
@@ -0,0 +1,14 @@
+#TODO Вывести первые N цисел кратные M и больше K
+
+list_numbers = [2, 3, 5, 7, 89, 4, 9, 7, 6, 54, 87, 69, 32, 33, 22, 34, 56, 38]
+list_result = []
+n = int(input('Input n: '))
+m = int(input('Input m: '))
+k = int(input('Input k: '))
+# print (n)
+# print(m)
+
+for number in range(n):
+    if number / m ==0 and number > k
+        list_result.append
+    # list_n[i] += 1
