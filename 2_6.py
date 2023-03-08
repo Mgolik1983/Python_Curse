57a02543d45915802549ca4f45e9abdf1c311f78
Mgolik16983 <mgolik1983@gmail.com>
2023-03-08 21:32:14 +0300

    Task 2_6

--- 2_6.py
+++ 2_6.py
@@ -1,1 +1,5 @@
 # Вывести четные числа от 2 до N по 5 в строку
+from itertools import zip_longest
+numbers = iter([*range(2, int(input('Input n: ')) + 1, 2)])
+numbers = list(zip_longest(numbers, numbers, numbers, numbers, numbers))
+print(numbers)
