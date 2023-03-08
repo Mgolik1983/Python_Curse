3f4542e19b42fa59735d756b88e62bdf0c5dbb88
Mgolik16983 <mgolik1983@gmail.com>
2023-03-08 15:46:43 +0300

    Task 2_5

--- 2_5.py
+++ 2_5.py
@@ -1,3 +1,16 @@
 # Сделать калькулятор: у пользователя
 # спрашивается число, потом действие и второе
 # число
+number_1 = float(input('Input number_1: '))
+number_2 = float(input('Input number_2: '))
+math_operation = input('Input math operation: ')
+if math_operation == '+':
+    print(f'result: {number_1 + number_2}')
+elif math_operation == '-':
+    print(f'result: {number_1 - number_2}')
+elif math_operation == '/':
+    print(f'result: {number_1 / number_2}')
+elif math_operation == '*':
+    print(f'result: {number_1 * number_2}')
+else:
+    print('Something wrong!')
