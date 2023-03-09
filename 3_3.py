3be10a3a7211eb67289df1c2cd2e5bb29bd36b15
Mgolik16983 <mgolik1983@gmail.com>
2023-03-09 22:46:27 +0300

    Task 3_3

--- 3_3.py
+++ 3_3.py
@@ -5,15 +5,16 @@ def shift_numbers(lst, n):
         n = abs(n)
         for i in range(n):
             lst.append(lst.pop(0))
-        else:
+    else:
+        for i in range(n):
             lst.insert(0, lst.pop())
 
 
 numbers = [1, 2, 3, 4, 5, 6, 7]
 print(numbers)
 
-shift_numbers(numbers, -2)
+shift_numbers(numbers, -3)
 print(numbers)
 
-shift_numbers(numbers, 3)
+shift_numbers(numbers, 2)
 print(numbers)
