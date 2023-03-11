f5cbb4758bcc02defdc16c71802d48b43bd1fd65
Mgolik16983 <mgolik1983@gmail.com>
2023-03-12 01:11:01 +0300

    Task 3_6

--- 3_6.py
+++ 3_6.py
@@ -6,5 +6,6 @@ def sort_even_odd(lst):
         lst.sort(key=lambda x: x % 2 == 0, reverse=True)
         return lst
 
+
 numbers = [35, 48, 36, 89, 101, 107, 56, 2, 4, 8, 78, 45, 77]
 print(sort_even_odd(numbers))
