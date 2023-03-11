8a708a7cd5a68e7cad8045b4f5ab9218b019a75e
Mgolik16983 <mgolik1983@gmail.com>
2023-03-12 01:08:34 +0300

    Task 3_8

--- 3_8.py
+++ 3_8.py
@@ -2,12 +2,15 @@
 #  поступает город, необходимо сказать из какой он страны
 
 def find_country():
- city = input('Input a city: ').title()
- Countries = {'Belarus': ['Minsk', 'Brest', 'Vitebsk', 'Gomel', 'Grodno', 'Mogilev'],
-              'Russia': ['Moscow', 'Sochi', 'Saint Petersburg', 'Novosibirsk', 'Volgograd'],
-              'USA': ['New York', 'Los Angeles', 'Chicago', 'Washington']}
- for name, contents in Countries.items():
-     if city in contents:
-         print(f'Your country is: {name}')
+    countries = {'Belarus': ['Minsk', 'Brest', 'Vitebsk', 'Gomel', 'Grodno', 'Mogilev'],
+                 'Russia': ['Moscow', 'Sochi', 'Saint Petersburg', 'Novosibirsk', 'Volgograd'],
+                 'USA': ['New York', 'Los Angeles', 'Chicago', 'Washington']}
+    city = str(input('Input a city: ').title())
+    for name, contents in countries.items():
+        if city in contents:
+            print(f'Your country is: {name}')
+        elif city not in contents:
+            print(f'Your country not found')
+
 
 find_country()
