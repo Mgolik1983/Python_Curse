6846b3af74052590aa76dbb908674554149fe0c9
Mgolik16983 <mgolik1983@gmail.com>
2023-03-11 21:08:27 +0300

    Task 3_5

--- 3_5.py
+++ 3_5.py
@@ -1,10 +1,8 @@
-#Дан список чисел, необходимо его развернуть без использования метода revese и
+# Дан список чисел, необходимо его развернуть без использования метода revese и
 # функции reversed, а так же дополнительного списка и среза
-
 def reverse_lst(lst):
     lst = [lst[-i] for i in range(1, len(lst) + 1)]
-    return(lst)
+    return lst
 
-numbers = [1, 2, 3, 4, 5, 6, 7]
-print(reverse_lst(numbers))
-
+numbers_lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(reverse_lst(numbers_lst))
