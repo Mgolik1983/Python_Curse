604355bc398d0a200392948e94fc78fbe7368a45
Mgolik16983 <mgolik1983@gmail.com>
2023-03-11 23:46:17 +0300

    Task 3_7

--- 3_7.py
+++ 3_7.py
@@ -2,4 +2,18 @@
 #  соседей, для крайних чисел одним из соседей является число с противоположной
 #  стороны списка
 
+def sum_numbers(lst):
+ res = []
+ for i in range(len(lst)):
+        if i == 0:
+            res.append(lst[i] + lst[-1])
+        elif i == len(lst) - 1:
+            res.append(lst[i-1] + lst[0])
+        else:
+            res.append(lst[i-1] + lst[i+1])
+ return res
+
+
 numbers = [35, 48, 36, 89, 101, 107, 56, 2, 4, 8, 78, 45, 77]
+
+print(sum_numbers(numbers))
