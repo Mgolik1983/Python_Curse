a2a6e55c1a03ee8676af5e769f1ca68f7563ad39
Mgolik16983 <mgolik1983@gmail.com>
2023-03-11 22:16:47 +0300

    Task 3_6

--- 3_6.py
+++ 3_6.py
@@ -1,9 +1,10 @@
 # Дан список рандомных чисел, необходимо отсортировать его в виде, сначала
-# четные, потом нечётные
+# четные, потом нечётные.
+
 def sort_even_odd(lst):
     for i in lst:
         lst.sort(key=lambda x: x % 2 == 0, reverse=True)
-    return lst
+        return lst
 
-numbers = [12, 13, 4, 7, 18, 21, 9, 56, 8, 9, 17, 29, 34]
+numbers = [35, 48, 36, 89, 101, 107, 56, 2, 4, 8, 78, 45, 77]
 print(sort_even_odd(numbers))
