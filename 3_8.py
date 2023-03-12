1862aa61b6ab8d1e03ee8d2038630f10c0cfe898
Mgolik16983 <mgolik1983@gmail.com>
2023-03-12 20:16:32 +0300

    Task 3_8

--- 3_8.py
+++ 3_8.py
@@ -5,13 +5,12 @@ def find_country():
     countries = {'Belarus': ['Minsk', 'Brest', 'Vitebsk', 'Gomel', 'Grodno', 'Mogilev'],
                  'Russia': ['Moscow', 'Sochi', 'Saint Petersburg', 'Novosibirsk', 'Volgograd'],
                  'USA': ['New York', 'Los Angeles', 'Chicago', 'Washington']}
-    city = str(input('Input a city: ').title())
+    city = input('Input a city: ').title()
     for name, contents in countries.items():
         if city in contents:
             print(f'Your country is: {name}')
-        elif city not in contents:
-            break
-    print(f'Your country not found')
+        raise ValueError
+
 
 
 find_country()
